bcdd79f93baeb72850d4ad44263c5792a0c2977e
Xikmatov <115355648+kamolidden@users.noreply.github.com>
2022-11-23 15:03:03 +0500

    Add files via upload

---
+++ javaScript/js/script.js
@@ -0,0 +1,16 @@
+// let obj = {
+//     name: "Kamoliddin",
+//     surname: "Hikamtov",
+//     age: "16",
+//     favColor: ["blue", "black"],
+//     edu: ["OWA", '5-maktab'],
+
+// }
+// console.log(obj);
+
+
+
+
+// let raq1 = prompt("birinchi nomer kiriting");
+// let raq2 = prompt("ikkinchi nomer kiriting");
+// console.log(raq1 % raq2);
